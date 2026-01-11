01aa72c1a08edad40f6c1079b79885f72c43e5a2
mkulpinsk <m.machowsk@gmail.com>
2026-01-11 15:53:36 +0100

    Implement  tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,50 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+// private Collection products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        this.addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        this.products.put(product, quantity);
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    private Map products = new HashMap<>();
+
+    public BigDecimal getNetValue() {
+        BigDecimal value = BigDecimal.ZERO;
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPrice();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(price);
+        }
+        return value;
     }
 
     public BigDecimal getTax() {
-        return null;
+
+        return getNetValue().subtract(getNetValue());
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossValue() {
+        BigDecimal value = BigDecimal.ZERO;
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPriceWithTax().substruct;
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(price);
+        }
+        return value;
     }
 }
